c270cd1f4c1a4781217195ef1d6129f53562e9c0
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-09-08 22:58:57 -0400

    added message reflecting HTTP Header paginated functionality

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction/asc";
@@ -9,6 +10,11 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginted issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -51,8 +57,23 @@ var displayIssues = function(issues) {
 
         // append to container
         issueEl.appendChild(typeEl);
+
+        // append to the DOM
+        issueContainerEl.appendChild(issueEl);
     }
-    issueContainerEl.appendChild(issueEl);
 };
 
-getRepoIssues("maxwillner/run-buddy");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("CherokeeLanguage/cherokeedictionary");
